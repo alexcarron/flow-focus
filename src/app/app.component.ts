5b90a4dea52a5b670f832278897100a4e88dcd2d
alexcarron <thealexcarron@gmail.com>
2024-07-07 19:20:28 -0500

    Modify app to display focus-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FocusPageComponent } from './focus-page/focus-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FocusPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'flow-focus';
 }
--- src/app/focus-page/focus-page.component.ts
+++ src/app/focus-page/focus-page.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import TasksManager from '../../model/TasksManager';
-import DeadlineTask from '../../model/DeadlineTask';
-import Task from '../../model/Task';
 
 @Component({
   selector: 'focus-page',
@@ -13,13 +11,4 @@ import Task from '../../model/Task';
 })
 export class FocusPageComponent {
 	tasksManager: TasksManager = TasksManager.getInstance();
-	task: Task = new DeadlineTask("Task Description", new Date(2024, 9,9));
-
-	ngOnInit() {
-		this.task.addStep("Step 1");
-		this.task.addStep("Step 2");
-		this.task.addStep("Step 3");
-
-		this.tasksManager.addTask(this.task);
-	}
 }
