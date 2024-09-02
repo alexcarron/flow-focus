0f44c47621b63bc0e6c3bf591cd4eb3077fd5934
alexcarron <thealexcarron@gmail.com>
2024-09-02 12:52:21 -0400

    Add content and style for skip task popup

--- src/app/base-popup/skip-task-popup/skip-task-popup.component.ts
+++ src/app/base-popup/skip-task-popup/skip-task-popup.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import Duration from '../../../model/time-management/Duration';
 import { BasePopupComponent } from '../base-popup.component';
+import { DurationInputComponent } from '../../input-controls/duration-input/duration-input.component';
 
 @Component({
   selector: 'skip-task-popup',
   standalone: true,
-  imports: [BasePopupComponent],
+  imports: [BasePopupComponent, DurationInputComponent],
   templateUrl: './skip-task-popup.component.html',
   styleUrl: './skip-task-popup.component.css'
 })
