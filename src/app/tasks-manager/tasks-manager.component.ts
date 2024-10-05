1e76d3c9986764f16d128104051a7ee32906f078
alexcarron <thealexcarron@gmail.com>
2024-10-05 14:42:53 -0400

    Add filter buttons to task manager

--- src/app/tasks-manager/tasks-manager.component.ts
+++ src/app/tasks-manager/tasks-manager.component.ts
@@ -16,15 +16,54 @@ enum Filter {
 	MustStartToday
 }
 
+enum SortBy {
+	Priority,
+	Name,
+	Steps,
+	TimeAvailable,
+	Duration,
+	RepeatInterval
+}
+
+enum SortDirection {
+	Ascending,
+	Descending
+}
+
 const filterToFunction: Record<Filter, (tasks: Task[]) => Task[]> = {
 	[Filter.None]: (tasks: Task[]) => tasks,
 	[Filter.Active]: (tasks: Task[]) => tasks.filter(
-		task => task.isActive(new Date())
+		task => task.isActive(new Date()) && task.getDeadline() !== null
 	),
 	[Filter.MustStartToday]: (tasks: Task[]) => tasks.filter(task =>
 		task.mustStartToday(new Date())
 	)
 };
+
+const sortByToFunction: Record<SortBy, (tasks: Task[], currentTime: Date) => Task[]> = {
+	[SortBy.Priority]: (tasks: Task[], currentTime: Date) => tasks,
+	[SortBy.Name]: (tasks: Task[], currentTime: Date) => [...tasks].sort((a, b) =>
+		a.getDescription().localeCompare(b.getDescription())
+	),
+	[SortBy.Steps]: (tasks: Task[], currentTime: Date) => [...tasks].sort((a, b) =>
+		a.getSteps().length - b.getSteps().length
+	),
+	[SortBy.TimeAvailable]: (tasks: Task[], currentTime: Date) => [...tasks].sort((a, b) =>
+		a.getTimeToComplete(currentTime) - b.getTimeToComplete(currentTime)
+	),
+	[SortBy.Duration]: (tasks: Task[], currentTime: Date) => [...tasks].sort((a, b) =>
+		a.getMaxRequiredTime(currentTime) - b.getMaxRequiredTime(currentTime)
+	),
+	[SortBy.RepeatInterval]: (tasks: Task[]) => [...tasks].sort((a, b) => {
+		if (a.getRepeatInterval() === null) {
+			return -1;
+		} else if (b.getRepeatInterval() === null) {
+			return 1;
+		} else {
+			return a.getRepeatInterval()! - b.getRepeatInterval()!;
+		}
+	})
+}
 
 @Component({
   selector: 'app-tasks-manager',
@@ -38,19 +77,43 @@ export class TasksManagerComponent {
 
 	tasksManager!: TasksManager;
 	currentFilter: Filter = Filter.None;
+	currentSortBy: SortBy = SortBy.Priority;
+	currentSortDirection: SortDirection = SortDirection.Ascending;
 
 	constructor(
-		private activatedRoute: ActivatedRoute
+		private readonly activatedRoute: ActivatedRoute
 	) {}
 
 	ngOnInit() {
 		this.tasksManager = this.activatedRoute.snapshot.data['tasksManager'];
+		this.resetSortButtonIcons();
+	}
+
+	resetSortButtonIcons() {
+		const unsorted_button_images = document.querySelectorAll('.unsorted');
+		const sort_button_images = document.querySelectorAll('.sorted-ascending, .sorted-descending');
+
+		unsorted_button_images.forEach(image => {
+			image.classList.remove('hidden');
+		})
+
+		sort_button_images.forEach(image => {
+			image.classList.add('hidden');
+		})
 	}
 
 	getTasks(): Task[] {
 		const tasksInPriorityOrder = this.tasksManager.getTasksInPriorityOrder(new Date());
+
+		const sortFunction = sortByToFunction[this.currentSortBy];
+		const sortedTasks = sortFunction(tasksInPriorityOrder, new Date());
+
+		if (this.currentSortDirection === SortDirection.Descending) {
+			sortedTasks.reverse();
+		}
+
 		const filterFunction = filterToFunction[this.currentFilter];
-		return filterFunction(tasksInPriorityOrder);
+		return filterFunction(sortedTasks);
 	}
 
 	getCurrentTime(): Date {
@@ -65,6 +128,45 @@ export class TasksManagerComponent {
 		const numFilters = Object.keys(Filter).length / 2;
 		const newFilterNum = (oldFilterNum + 1) % numFilters;
 		this.currentFilter = newFilterNum;
+	}
+
+	toggleColumnSort(onClickEvent: MouseEvent, column_num: number) {
+		const oldSortBy = this.currentSortBy;
+		this.currentSortBy = column_num as SortBy;
+
+		if (oldSortBy !== this.currentSortBy) {
+			this.currentSortDirection = SortDirection.Ascending;
+		}
+		else {
+			this.currentSortDirection =
+				this.currentSortDirection === SortDirection.Ascending
+					? SortDirection.Descending
+					: SortDirection.Ascending
+		}
+
+		// Find images inside clicked button
+		const imgElement = onClickEvent.target as HTMLImageElement
+		const buttonElement = imgElement.parentElement as HTMLButtonElement
+
+		this.resetSortButtonIcons()
+		const unsorted_image = buttonElement.querySelector(".unsorted");
+		const sorted_asc_image = buttonElement.querySelector(".sorted-ascending");
+		const sorted_desc_image = buttonElement.querySelector(".sorted-descending");
+
+		console.log({
+			buttonElement,
+			unsorted_image,
+			sorted_asc_image,
+			sorted_desc_image,
+		})
+
+		unsorted_image?.classList.add('hidden');
+		if (this.currentSortDirection === SortDirection.Ascending) {
+			sorted_asc_image?.classList.remove('hidden');
+		}
+		else {
+			sorted_desc_image?.classList.remove('hidden');
+		}
 	}
 
 	willTaskBeAlwaysAvailable(task: Task): boolean {
