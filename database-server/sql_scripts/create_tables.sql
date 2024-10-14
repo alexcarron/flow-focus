566b19f7dcf0147929deda46d9371d057020601b
alexcarron <thealexcarron@gmail.com>
2024-10-14 17:16:23 -0400

    Add database schema file

---
+++ database-server/sql_scripts/create_tables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS tasks (
+  id SERIAL PRIMARY KEY,
+  action VARCHAR(512) NOT NULL,
+	start_time TIMESTAMP WITH TIME ZONE,
+	end_time TIMESTAMP WITH TIME ZONE,
+	deadline TIMESTAMP WITH TIME ZONE,
+	min_duration INTERVAL,
+	max_duration INTERVAL,
+	repeat_interval INTERVAL,
+	is_mandatory BOOLEAN DEFAULT FALSE,
+	is_complete BOOLEAN DEFAULT FALSE,
+	is_skipped BOOLEAN DEFAULT FALSE,
+	last_actioned_step_id INT REFERENCES steps(id)
+
+	CHECK (
+		(min_duration IS NULL OR max_duration IS NULL) OR
+		(min_duration <= max_duration)
+	),
+	CHECK (
+		(start_time IS NULL OR end_time IS NULL) OR
+		(start_time <= end_time)
+	),
+	CHECK (
+		(start_time IS NULL OR deadline IS NULL) OR
+		(start_time <= deadline)
+	),
+	CHECK (
+		(deadline IS NULL OR end_time IS NULL) OR
+		(deadline <= end_time)
+	),
+);
+
+CREATE TYPE IF NOT EXISTS step_status AS ENUM (
+	'COMPLETED',
+	'SKIPPED',
+	'UNCOMPLETED'
+);
+
+CREATE TABLE IF NOT EXISTS steps (
+	task_id INT NOT NULL REFERENCES tasks(id),
+	position INT UNIQUE NOT NULL CHECK (position > 0),
+	instruction VARCHAR(512) NOT NULL,
+	status step_status DEFAULT 'UNCOMPLETED' NOT NULL,
+
+	PRIMARY KEY (task_id, index)
+)
+
+
+
+
