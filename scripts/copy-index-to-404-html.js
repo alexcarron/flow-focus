ef3295950dbfb0f077f52b85e9f705a359e07833
alexcarron <thealexcarron@gmail.com>
2026-09-11 09:48:26 -0400

    Fix 404 html path being same as index path in copy-index-to-404-html.js script

--- scripts/copy-index-to-404-html.js
+++ scripts/copy-index-to-404-html.js
@@ -3,6 +3,6 @@ import { join } from 'path';
 
 const distDirectory = join(import.meta.dirname, '..', 'dist');
 const HTMLIndexPath = join(distDirectory, 'index.html');
-const HTML404Path = join(distDirectory, 'index.html');
+const HTML404Path = join(distDirectory, '404.html');
 
 copyFileSync(HTMLIndexPath, HTML404Path);
